b9a4d9adf9d50f27da8440c90ff48da6fd82d78c
Mohanraj48 <mohanbca48@gmail.com>
2019-02-12 21:32:48 +0530

    My Homepage

---
+++ Google/src/test/java/com/google/homePage/GoogleHome.java
@@ -0,0 +1,27 @@
+package com.google.homePage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleHome {
+	
+	@Test
+	public void Homepage() throws Exception
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "E:/Mohan/jarfiles/WebDrivers/Chrome/chromedriver_win32/chromedriver.exe");
+		WebDriver d=new ChromeDriver();
+		d.manage().window().maximize();
+		d.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		d.get("https://www.google.co.in");
+		System.out.println(d.getTitle());
+		
+	Thread.sleep(2000);
+	d.quit();
+		
+	}
+
+}
